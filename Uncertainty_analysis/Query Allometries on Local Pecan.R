f150e47d080baefc2ea596d3e090bcb5c1d49bc9
crollinson <crollinson@gmail.com>
2015-03-24 12:08:42 -0400

    testing of new DBH feature in Pecan
    Scripts for the testing & diagnosing of adding a DBH range to the
    allometry functions in pecan.  As of 24 Mar 2015, this feature of pecan
    only exists in C. Rollinson’s DBH-cutoff branch of Pecan

--- Uncertainty_analysis/Query Allometries on Local Pecan.R
+++ Uncertainty_analysis/Query Allometries on Local Pecan.R
@@ -1,7 +1,7 @@
 # Script Querying allometries
 setwd("~/Desktop/pecan/modules/allometry/R")
 
-outdir <- "~/Dropbox/PalEON CR/Tree Rings/Tree-Rings-and-Biomass/Uncertainty_analysis/AllomFiles/March19" # CR Office
+outdir <- "~/Dropbox/PalEON CR/Tree Rings/Tree-Rings-and-Biomass/Uncertainty_analysis/AllomFiles" # CR Office
 #outdir <- "~/Desktop/PalEON CR/Tree Rings/Tree-Rings-and-Biomass/Uncertainty_analysis/AllomFiles" # CR Laptop
 
 source("AllomAve.R")
@@ -23,31 +23,33 @@ source("read.allom.data.R")
 # # AllomAve(pipo,2,outdir=file.path(outdir, "TysonOnly"),parm="../data/Table3_GTR-NE-319.v2_RossAdendum.csv",ngibbs=500)
 
 
-# # Example with just 1 eq
-# picea = list(test = data.frame(spcd=90,acronym="PICEA"))
+# Example with just 1 eq
+picea = list(Picea.Bad.Eq = data.frame(spcd=90,acronym="PICEA"))
 
-# AllomAve(picea,2,outdir=file.path(outdir, "March19"),parm="../data/Table3_GTR-NE-319.v2_RossAdendum.csv",ngibbs=500)
+AllomAve(picea,2,outdir=file.path(outdir, "Picea"),parm="../data/Table3_GTR-NE-319.v2_RossAdendum.csv",ngibbs=500)
+
+AllomAve(picea,2,outdir=file.path(outdir, "Picea"),parm="../data/Table3_GTR-NE-319.v2_RossAdendum.csv",ngibbs=500, dmin=2, dmax=250)
 
 
-# Species level equation "if available" for Ross and Marcy Valles Data'
-valles.sp = list(PIPO = data.frame(spcd=122, acronym="PIPO"),
-              PIEN = data.frame(spcd=93, acronym="PIEN"),
-              PSME = data.frame(spcd=202, acronym="PSME"),
-              ABCO = data.frame(spcd=15, acronym = "ABCO"))
-AllomAve(valles.sp,2,outdir=outdir,parm="../data/Table3_GTR-NE-319.v2_RossAdendum.csv",ngibbs=1000)
+# # # Species level equation "if available" for Ross and Marcy Valles Data'
+# valles.sp = list(PIPO = data.frame(spcd=122, acronym="PIPO"),
+              # PIEN = data.frame(spcd=93, acronym="PIEN"),
+              # PSME = data.frame(spcd=202, acronym="PSME"),
+              # ABCO = data.frame(spcd=15, acronym = "ABCO"))
+# AllomAve(valles.sp,2,outdir=outdir,parm="../data/Table3_GTR-NE-319.v2_RossAdendum.csv",ngibbs=1000)
 
-valles.sp2 = list(PIPO = data.frame(spcd=122, acronym="PIPO"))
-AllomAve(valles.sp2,2,outdir= outdir,parm="../data/Table3_GTR-NE-319.v2_RossAdendum.csv",ngibbs=1000)
+# valles.sp2 = list(PIPO = data.frame(spcd=122, acronym="PIPO"))
+# AllomAve(valles.sp2,2,outdir= outdir,parm="../data/Table3_GTR-NE-319.v2_RossAdendum.csv",ngibbs=1000)
 
-# Genus level equations for Ross's valles data'
-valles.genus = list(pinus.sp = data.frame(spcd=100, acronym="PINUS"),
-                 picea.sp = data.frame(spcd=90, acronym="PICEA"),
-                 abies.sp = data.frame(spcd=10, acronym="ABIES"))
-AllomAve(valles.genus,2,outdir= outdir,parm="../data/Table3_GTR-NE-319.v2_RossAdendum.csv",ngibbs=1000)
+# # Genus level equations for Ross's valles data'
+# valles.genus = list(pinus.sp = data.frame(spcd=100, acronym="PINUS"),
+                 # picea.sp = data.frame(spcd=90, acronym="PICEA"),
+                 # abies.sp = data.frame(spcd=10, acronym="ABIES"))
+# AllomAve(valles.genus,2,outdir= outdir,parm="../data/Table3_GTR-NE-319.v2_RossAdendum.csv",ngibbs=1000)
 
-valles.genus2 = list(picea.sp = data.frame(spcd=90, acronym="PICEA"))
-AllomAve(valles.genus2,2,outdir= outdir,parm="../data/Table3_GTR-NE-319.v2_RossAdendum.csv",ngibbs=1000)
+# valles.genus2 = list(picea.sp = data.frame(spcd=90, acronym="PICEA"))
+# AllomAve(valles.genus2,2,outdir= outdir,parm="../data/Table3_GTR-NE-319.v2_RossAdendum.csv",ngibbs=1000)
 
-#making up our own PFT's, just need a species--query simon's database adn cross list based on christy's code
-#pfts = list(PFT.TITLE = data.frame(spcd=PFT.TITLE$spcd,acronym=PFT.TITLE$acronym))
+# #making up our own PFT's, just need a species--query simon's database adn cross list based on christy's code
+# #pfts = list(PFT.TITLE = data.frame(spcd=PFT.TITLE$spcd,acronym=PFT.TITLE$acronym))
 
